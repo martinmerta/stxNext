22796af7d94ddf8d0d95149bc267ab5571e87b01
martinmerta <mmerta4@wp.pl>
2019-09-24 16:34:11 +0200

    app source files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import FetchBooks from './components/FetchBooks';
 import './App.css';
-
 function App() {
+  const [searchBy, setSearchValue] = useState('intitle');
+  const [inputValue, setInputValue] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='helloTitle'>
+        Hello, welcome to the best book searching site ever!
+      </h1>
+      <form className='form'>
+        <label>
+          Type what you want to search:
+          <input
+            type='text'
+            name='inputValue'
+            value={inputValue}
+            onChange={e => {
+              const text = e.currentTarget.value;
+              setInputValue(text);
+            }}
+          />
+        </label>
+        <label>
+          Search by:
+          <select
+            value={searchBy}
+            onChange={e => {
+              const value = e.currentTarget.value;
+              setSearchValue(value);
+              setInputValue('');
+            }}
+          >
+            <option value='intitle'>title</option>
+            <option value='inauthor'>author</option>
+            <option value='inpublisher'>publisher</option>
+            <option value='isbn'>ISBN number</option>
+          </select>
+        </label>
+      </form>
+      {inputValue !== '' ? (
+        <FetchBooks searchBy={searchBy} inputValue={inputValue} />
+      ) : null}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+  shallow(<App />);
+});
+it('contain form', () => {
+  const app = shallow(<App />);
+  expect(app.containsMatchingElement([<form />])).toEqual(true);
 });
