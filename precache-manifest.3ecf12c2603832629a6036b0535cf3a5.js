f28f16578fb5d8359edb59f304b825469ca4606c
martinmerta <mmerta4@wp.pl>
2019-09-24 19:53:37 +0200

    Updates

---
+++ precache-manifest.3ecf12c2603832629a6036b0535cf3a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e1b175fb6be4b64db2b835930b9b5b",
+    "url": "/stxNext/index.html"
+  },
+  {
+    "revision": "0f477e5fc00fe65f0e89",
+    "url": "/stxNext/static/css/main.3af8872d.chunk.css"
+  },
+  {
+    "revision": "c0923ee04d7b28a45ac2",
+    "url": "/stxNext/static/js/2.6ca44d42.chunk.js"
+  },
+  {
+    "revision": "0f477e5fc00fe65f0e89",
+    "url": "/stxNext/static/js/main.b10453fa.chunk.js"
+  },
+  {
+    "revision": "7e5ae087a420df912098",
+    "url": "/stxNext/static/js/runtime-main.645d9ea6.js"
+  }
+]);
